5dde05f0c8136503dfe41c412d1a32cc2797ef38
Yi-Fan Feng <76724832+Evan102@users.noreply.github.com>
2023-03-03 18:12:20 +0800

    DDQN_clipping

---
+++ DDQN_clipping/Buffer.py
@@ -0,0 +1,60 @@
+import collections
+import typing
+
+import numpy as np
+
+
+_field_names = [
+    "state",
+    "action",
+    "reward",
+    "next_state",
+    "done"
+]
+Experience = collections.namedtuple("Experience", field_names=_field_names)
+
+
+class ExperienceReplayBuffer:
+    """Fixed-size buffer to store Experience tuples."""
+
+    def __init__(self,
+                 batch_size: int,
+                 buffer_size: int = None,
+                 random_state: np.random.RandomState = None) -> None:
+        """
+        Initialize an ExperienceReplayBuffer object.
+
+        Parameters:
+        -----------
+        buffer_size (int): maximum size of buffer
+        batch_size (int): size of each training batch
+        random_state (np.random.RandomState): random number generator.
+        
+        """
+        self._batch_size = batch_size
+        self._buffer_size = buffer_size
+        self._buffer = collections.deque(maxlen=buffer_size)
+        self._random_state = np.random.RandomState() if random_state is None else random_state
+        
+    def __len__(self) -> int:
+        return len(self._buffer)
+    
+    @property
+    def batch_size(self) -> int:
+        """Number of experience samples per training batch."""
+        return self._batch_size
+    
+    @property
+    def buffer_size(self) -> int:
+        """Total number of experience samples stored in memory."""
+        return self._buffer_size
+    
+    def append(self, experience: Experience) -> None:
+        """Add a new experience to memory."""
+        self._buffer.append(experience)
+    
+    def sample(self) -> typing.List[Experience]:
+        """Randomly sample a batch of experiences from memory."""
+        idxs = self._random_state.randint(len(self._buffer), size=self._batch_size)
+        experiences = [self._buffer[idx] for idx in idxs]
+        return experiences
